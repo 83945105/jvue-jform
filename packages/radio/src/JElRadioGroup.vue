c3e3b2347f2a4fad5a9eff73176e92c905e9e96d
18606167809@163.com <BAI_chao910405>
2020-12-19 21:33:51 +0800

    1、调整JElRadioGroup控件dataProp配置方式

--- packages/radio/src/JElRadioGroup.vue
+++ packages/radio/src/JElRadioGroup.vue
@@ -4,108 +4,108 @@
       <el-radio-button v-if="mode === 'button'"
                        :key="$index"
                        :label="option.value"
-                       :disabled="option.disabled">{{option.label}}
+                       :disabled="option.disabled">{{ option.label }}
       </el-radio-button>
       <el-radio v-else
                 :key="$index"
                 :label="option.value"
                 :disabled="option.disabled"
-                :border="option.border|| border">{{option.label}}
+                :border="option.border|| border">{{ option.label }}
       </el-radio>
     </template>
   </el-radio-group>
 </template>
 
 <script>
-  import {isObject} from "../../../src/utils/util";
+import {isObject} from "../../../src/utils/util";
 
-  export default {
-    name: "j-el-radio-group",
+export default {
+  name: "j-el-radio-group",
 
-    props: {
-      value: [String, Number, Boolean],   // 绑定值
-      size: String,                       // 单选框组尺寸，仅对按钮形式的 Radio 或带有边框的 Radio 有效
-      disabled: Boolean,                  // 是否禁用
-      textColor: {                        // 按钮形式的 Radio 激活时的文本颜色
-        type: String,
-        default: '#ffffff'
-      },
-      fill: {                             // 按钮形式的 Radio 激活时的填充色和边框色
-        type: String,
-        default: '#409EFF'
-      },
+  props: {
+    value: [String, Number, Boolean],   // 绑定值
+    size: String,                       // 单选框组尺寸，仅对按钮形式的 Radio 或带有边框的 Radio 有效
+    disabled: Boolean,                  // 是否禁用
+    textColor: {                        // 按钮形式的 Radio 激活时的文本颜色
+      type: String,
+      default: '#ffffff'
+    },
+    fill: {                             // 按钮形式的 Radio 激活时的填充色和边框色
+      type: String,
+      default: '#409EFF'
+    },
 
-      border: Boolean,                    // 是否显示边框
-      mode: {                             // 模式
-        type: String,
-        validator(value) {
-          return [null, 'button'].indexOf(value) !== -1
-        }
-      },
-
-      data: Array,                        // 选项数据
-      dataProp: {                         // 数据属性
-        type: Object,
-        default() {
-          return {
-            labelKey: 'label',      // 用于解析选项标签的key
-            valueKey: 'value',      // 用于解析选项值的key
-            disabledKey: 'disabled',// 用于解析选项是否禁用的key
-            borderKey: 'border'     // 用于解析选项是否有边框的key
-          };
-        },
-        validator(value) {
-          return isObject(value);
-        }
+    border: Boolean,                    // 是否显示边框
+    mode: {                             // 模式
+      type: String,
+      validator(value) {
+        return [null, 'button'].indexOf(value) !== -1
       }
     },
 
-    data() {
-      return {
-        value_: null,
-        data_: []
-      };
-    },
-
-    computed: {
-      bind__() {
+    data: Array,                        // 选项数据
+    dataProp: {                         // 数据属性
+      type: Object,
+      default() {
         return {
-          size: this.size,
-          disabled: this.disabled,
-          textColor: this.textColor,
-          fill: this.fill
+          label: 'label',      // 用于解析选项标签的key
+          value: 'value',      // 用于解析选项值的key
+          disabled: 'disabled',// 用于解析选项是否禁用的key
+          border: 'border'     // 用于解析选项是否有边框的key
         };
+      },
+      validator(value) {
+        return isObject(value);
+      }
+    }
+  },
+
+  data() {
+    return {
+      value_: null,
+      data_: []
+    };
+  },
+
+  computed: {
+    bind__() {
+      return {
+        size: this.size,
+        disabled: this.disabled,
+        textColor: this.textColor,
+        fill: this.fill
+      };
+    }
+  },
+
+  watch: {
+    value: {
+      immediate: true,
+      handler(val) {
+        this.value_ = val;
       }
     },
-
-    watch: {
-      value: {
-        immediate: true,
-        handler(val) {
-          this.value_ = val;
-        }
-      },
-      value_: {
-        immediate: true,
-        handler(val) {
-          this.$emit('input', val);
-        }
-      },
-      data: {
-        immediate: true,
-        handler(val) {
-          this.data_ = (val || []).map(option => {
-            return {
-              label: option[this.dataProp.labelKey],
-              value: option[this.dataProp.valueKey],
-              disabled: !!option[this.dataProp.disabledKey],
-              border: !!option[this.dataProp.borderKey]
-            };
-          });
-        }
+    value_: {
+      immediate: true,
+      handler(val) {
+        this.$emit('input', val);
+      }
+    },
+    data: {
+      immediate: true,
+      handler(val) {
+        this.data_ = (val || []).map(option => {
+          return {
+            label: option[this.dataProp.label],
+            value: option[this.dataProp.value],
+            disabled: !!option[this.dataProp.disabled],
+            border: !!option[this.dataProp.border]
+          };
+        });
       }
     }
   }
+}
 </script>
 
 <style scoped>
--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,54 @@
 <template>
   <div id="app">
+    <j-el-radio-group :data="radioGroupData" :data-prop="{label: 'label2', value: 'value2'}"></j-el-radio-group>
     <j-render :data="formData"></j-render>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data() {
-      return {
-        formData: {
-          tag: 'j-el-form',
-          children: [
-            {
-              tag: 'j-el-description'
-            },
-            {
-              tag: 'j-el-editor'
-            },
-            {
-              tag: 'j-el-icon-picker'
-            },
-            {
-              tag: 'j-el-coordinate-picker'
-            }
-          ]
+export default {
+  name: 'app',
+  data() {
+    return {
+      radioGroupData: [
+        {
+          label2: '张三',
+          value2: '1'
+        },
+        {
+          label2: '李四',
+          value2: '2'
         }
+      ],
+      formData: {
+        tag: 'j-el-form',
+        children: [
+          {
+            tag: 'j-el-description'
+          },
+          {
+            tag: 'j-el-editor'
+          },
+          {
+            tag: 'j-el-icon-picker'
+          },
+          {
+            tag: 'j-el-coordinate-picker'
+          }
+        ]
+      }
+    }
+  },
+
+  watch: {
+    radioGroupData: {
+      immediate: true,
+      handler(val) {
+        console.log(val)
       }
     }
   }
+}
 </script>
 
 <style>
